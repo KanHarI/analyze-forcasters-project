fe66da9fdad12c22053adaf5292caad5cbd37182
Itay Knaan Harpaz <itay_knaan-harpaz@saips.co.il>
2020-12-30 00:37:17 +0200

    Added readme and installation

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # analyze-forcasters-project
+
+Running:
+* Create a venv
+* Install package
+* Run
+```
+py -3 -m venv venv
+pip install .
+analyze_forecasts --predictions-csv data\ForecastsAnonymized.csv --truth-csv data\GroundTruth.csv --end-only-1st-pred F217 --start-only-2nd-pred F260 --size-1st-questions 59 --size-2nd-questions 39
+```
--- setup.py
+++ setup.py
@@ -9,8 +9,13 @@ setup(
     version=__version__,
     description="Analysis of the forecasters project",
     install_requires=['pandas', 'numpy', 'scipy', 'matplotlib'],
-    entry_points={},
+    entry_points={
+        "console_scripts": [
+            "analyze_forecasts=analyzer_main.main:main"
+        ]
+    },
     author="Itay Knaan Harpaz",
     author_email="knaan.harpaz@gmail.com",
     url="https://github.com/KanHarI/analyze-forecasters-project",
+    packages=find_packages(),
 )
